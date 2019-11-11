544873ce8e9e5e8189dccb7330d84c15ed40339e
monologg <adieujw@gmail.com>
2019-11-11 10:19:43 +0900

    Add fully connected layer

--- model.py
+++ model.py
@@ -3,6 +3,20 @@ import torch.nn as nn
 from transformers import BertModel
 
 
+class FCLayer(nn.Module):
+    def __init__(self, input_dim, output_dim, use_activation=False):
+        super(FCLayer, self).__init__()
+        self.use_activation = use_activation
+        self.dropout = nn.Dropout(dropout_rate)  # TODO: gin-config
+        self.linear = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        x = self.dropout(x)
+        if self.use_activation:
+            x = torch.tanh(x)
+        return self.linear(x)
+
+
 class RBERT(nn.Module):
     def __init__(self):
         super(RBERT, self).__init__()
